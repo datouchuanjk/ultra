b0a5d0ab8c76ffec28eb04283b38358fc0f1635f
datouchuanjk <13720327027@163.com>
2025-04-02 20:34:22 +0800

    2

--- foundation/develop/src/main/java/io/datou/develop/Intent.kt
+++ foundation/develop/src/main/java/io/datou/develop/Intent.kt
@@ -1,6 +1,8 @@
 package io.datou.develop
 
 import android.app.Activity
+import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Parcelable
@@ -8,61 +10,43 @@ import androidx.core.net.toUri
 import java.io.File
 import java.io.Serializable
 
-val Intent.isNullActivity get() = resolveActivity(Instance.packageManager) == null
+fun Intent.takeIfNotNullActivity() = takeIf { resolveActivity(Instance.packageManager) != null }
 
 inline fun <reified T : Activity> intentOf(
     block: Intent.() -> Unit = {}
 ) = Intent(Instance, T::class.java).apply(block)
 
-fun installApk(
+fun Context.installApk(
     apkFile: File,
     authority: String = "${Instance.packageName}.fileProvider"
-) = Instance.startActivity(
-    Intent(Intent.ACTION_VIEW).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        setDataAndType(
-            apkFile.toProviderUri(authority),
-            "application/vnd.android.package-archive"
-        )
+) = Intent(Intent.ACTION_VIEW).apply {
+    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    setDataAndType(
+        apkFile.toProviderUri(authority),
+        "application/vnd.android.package-archive"
+    )
+}.takeIfNotNullActivity()
+    ?.run {
+        startActivity(this)
     }
-)
 
-fun jumpToBrowser(url: String) = Instance.startActivity(
-    Intent(Intent.ACTION_VIEW).apply {
-        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        setData(url.toUri())
+fun Context.jumpToBrowser(url: String) = Intent(Intent.ACTION_VIEW, url.toUri())
+    .takeIfNotNullActivity()
+    ?.run {
+        startActivity(this)
     }
-)
 
-fun String.shareTo(
-    packageName: String? = null,
-    openShareFailed: () -> Unit = {}
-) {
-    var intent = Intent(Intent.ACTION_SEND)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    intent.type = "text/plain"
-    intent.putExtra(Intent.EXTRA_TEXT, this)
-    val isAppInstalled = if (packageName.isNullOrEmpty()) {
-        intent = Intent.createChooser(intent, "share to")
-        true
-    } else {
-        try {
-            Instance.packageManager.getPackageInfo(packageName, 0)
-            intent.setPackage(packageName)
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
+fun Context.shareTo(
+    text: String,
+    packageName: String
+) = Intent(Intent.ACTION_SEND).apply {
+    type = "text/plain"
+    putExtra(Intent.EXTRA_TEXT, text)
+    setPackage(packageName)
+}.takeIfNotNullActivity()
+    ?.run {
+        startActivity(this)
     }
-    if (isAppInstalled && !intent.isNullActivity) {
-        Instance.startActivity(intent)
-    } else {
-        openShareFailed()
-    }
-}
-
 
 
 
