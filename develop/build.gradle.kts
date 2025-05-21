a3cabaab710ba8fb02d418340cc55c2aa484a2c0
huang <13720327027@16.com>
2025-05-21 22:50:37 +0800

    1

--- develop/build.gradle.kts
+++ develop/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    kapt(libs.hilt.compiler)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -34,14 +34,14 @@ dependencies {
     //basic
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
-    //compose
+    // ⬇️  Compose 依赖
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
 
-    // ⬇️ 新增 Hilt 依赖
+    // ⬇️  Hilt 依赖
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     kapt(libs.hilt.compiler)
