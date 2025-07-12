a8ff8847edd238896edec02cbe1dab4e220dbe6b
13720 <13720327027@163.com>
2025-07-12 11:11:04 +0800

    123

--- banner/build.gradle.kts
+++ banner/build.gradle.kts
@@ -1,11 +1,10 @@
-import org.gradle.internal.impldep.com.jcraft.jsch.JSch.setConfig
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    `maven-publish`
-    id("signing")
+    id("com.vanniktech.maven.publish") version "0.32.0"
 }
 android {
     namespace = "io.watermelon.banner"
@@ -30,51 +29,37 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "io.github.datouchuanjk"
-            artifactId = "banner"
-            version = "1.0.0"
-            afterEvaluate {
-                from(components["release"])
-            }
-            pom {
-                name.set(" Banner ")
-                description.set("A reusable banner component for Android applications")
-                url.set("https://gitee.com/datouchuanjk/develop/tree/master/banner")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("datouchuanjk")
-                        name.set("datouchuanjk")
-                        email.set("13720327027@163.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://gitee.com/datouchuanjk/Develop.git")
-                    developerConnection.set("scm:git:git@gitee.com:datouchuanjk/Develop.git")
-                    url.set("https://gitee.com/datouchuanjk/develop/tree/master/banner")
-                }
+mavenPublishing{
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates("io.github.datouchuanjk", "banner", "1.0.0")
+    pom{
+        name.set("io.github.datouchuanjk")
+        description.set("banner")
+        inceptionYear.set("2025")
+        url.set("https://gitee.com/datouchuanjk/Develop.git")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
-    }
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = "zaskSVX8"
-                password = "C1q4QjzojZ0u3LcYHkP8CzlpCSIJLl2axqkObyaTR+3i"
+        developers {
+            developer {
+                id.set("datouchuanjk")
+                name.set("datouchuanjk")
+                email.set("13720327027@163.com")
+                url.set("https://gitee.com/datouchuanjk")
             }
+        }
+        scm {
+            url.set("https://gitee.com/datouchuanjk/develop/tree/master/banner")
+            connection.set("scm:git:https://gitee.com/datouchuanjk/Develop.git")
+            developerConnection.set("scm:git:git@gitee.com:datouchuanjk/Develop.git")
         }
     }
 }
 
-signing {
-    sign(publishing.publications["release"])
-}
+
+
