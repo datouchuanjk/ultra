63f84b687568dd41a9c1c89cb342de73aad2ffc4
13720 <13720327027@163.com>
2025-07-12 17:17:27 +0800

    first
    Signed-off-by: 13720 <13720327027@163.com>

---
+++ banner/build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":":banner","url":"index.html"}]
---
+++ banner/build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"io.watermelon.banner","url":"io/watermelon/banner/package-summary.html"}, {"l":"All packages","url":"index.html"}]
